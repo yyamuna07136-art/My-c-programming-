80696b6813b30fcf5629bd4f84f4a63bd0334bdd
yyamuna07136-art <yyamuna07136@gmail.com>
2025-11-18 00:57:26 +0530

    Add README for reading int and float values
    Fix formatting issues and correct scanf format specifiers.

---
+++ Read int and float.c
@@ -0,0 +1,12 @@
+//🌅 read a integer and float value
+#include<stdio.h>
+int  main()
+{ int a;
+float b;
+printf ("enter an integer:");
+scanf ("%d",&a);
+printf ("enter a float:");
+scanf ("%d",&b);
+printf("\n you entered integer%d\n");
+printf ("\n you entered float%.2f\n");
+}
