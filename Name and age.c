0ce777d1c775a69dad973c129355359f3439ce73
yyamuna07136-art <yyamuna07136@gmail.com>
2025-11-18 00:00:32 +0530

    Create Name and age.c for user input
    Fixes issues with name and age input/output.

---
+++ Name and age.c
@@ -0,0 +1,13 @@
+//print your name,age
+#include<stdio.h>
+int main()
+{ char name;
+int age;
+printf ("enter a name \n");
+scanf("%c\n",&name);
+
+printf ("enter a age\n");
+scanf ("%d\n",age);
+printf ("hello(name),your are(age)years old\n");
+
+}
