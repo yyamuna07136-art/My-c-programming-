737ff1a4e8e28ae7b42427f0b7dca1273e1d7d37
yyamuna07136-art <yyamuna07136@gmail.com>
2025-11-18 23:44:19 +0530

    Create Control flow loop,3.c

---
+++ Control flow loop,3.c
@@ -0,0 +1,14 @@
+// positive,negative,zero
+#include <stdio.h>
+int main() {
+    int number;
+    printf("Enter  an integer:");
+    scanf("%d",&number);
+    if (number > 0) {
+        printf (" The number is positive.\n");
+    } else if (number < 0) {
+        printf ("The number is negative.\n");
+    } else {
+        printf ("The number is zero.\n");
+    }
+}          
