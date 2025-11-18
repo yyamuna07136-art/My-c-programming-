48ad7a0e8c6576831179a72df5e510f831c94dec
yyamuna07136-art <yyamuna07136@gmail.com>
2025-11-18 11:29:25 +0530

    Add nested if-else grading program
    Implement nested if-else structure for grading based on marks.

---
+++ Nested if else program.c
@@ -0,0 +1,24 @@
+// nested if else   program 
+#include <stdio.h>
+int main()
+{ int marks;
+    
+    printf("enter your marks:");
+    scanf ("%d",& marks);
+    if(marks>=50){
+       if(marks>=90){
+        printf ("grade:A\n");
+    } else  if ( marks>=75){
+        printf (" grade:B\n");
+    } else {
+        printf ("grade:C\n");
+    } 
+    } else { printf (" grade:F\n");
+        
+    } 
+      
+    }
+    
+
+
+    
