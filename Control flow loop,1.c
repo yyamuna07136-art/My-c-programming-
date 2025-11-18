cc6934fde9bbf3c1fdacecd43b2f395331c929af
yyamuna07136-art <yyamuna07136@gmail.com>
2025-11-18 23:35:17 +0530

    Add even or odd checker program in C

---
+++ Control flow loop,1.c
@@ -0,0 +1,14 @@
+// even or odd checker
+#include <stdio.h>
+int main() {
+    int number;
+    printf("Enter  an integer:");
+    scanf("%d",&number);
+    if (number % 2 == 0) {
+        printf ("%d is an even integer.\n", number);
+    } else {
+        printf ("%d is an odd integer.\n", number);
+    }
+}
+            
+            
