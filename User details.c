e52bbbbb8f5acdae0713b3dc0ee86049586022b7
yyamuna07136-art <yyamuna07136@gmail.com>
2025-11-18 16:30:29 +0530

    Create User details.c

---
+++ User details.c
@@ -0,0 +1,21 @@
+//user details 
+#include <stdio.h>
+int userdetails()
+{ char name , depar;
+int age;
+{ printf ("enter your name :\n");
+scanf("%s",&name);
+  printf ("enter your  age :\n");
+  scanf("%d",&age);
+  printf (" enter your department :\n");
+  scanf("%s",&depar);
+}
+
+
+int main() {
+userdetails ();}
+
+
+
+
+    
