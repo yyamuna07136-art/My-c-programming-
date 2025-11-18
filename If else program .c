4efa5d8911eb19ac05d27f91b8ec9b986fc30607
yyamuna07136-art <yyamuna07136@gmail.com>
2025-11-18 11:04:03 +0530

    Add if-else program to check even or odd integers
    Implement if-else program to check even or odd integers.

---
+++ If else program .c
@@ -0,0 +1,15 @@
+// if  else program 
+#include <stdio.h>
+int main()
+{ int number;
+    
+    printf("enter an integer:");
+    scanf ("%d",&number);
+    if(number%2==0){
+        printf ("%d is an even integer\n",number);
+    } else {
+        printf ("%d is an odd integer \n", number);
+    }
+    }
+
+    
