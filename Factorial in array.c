179863456a1cf2667d786a9244d9cf10aac158a7
yyamuna07136-art <yyamuna07136@gmail.com>
2025-11-19 15:57:39 +0530

    Create Factorial in array.c

---
+++ Factorial in array.c
@@ -0,0 +1,12 @@
+//factorial  in the array 
+#include <stdio.h>
+int main() {
+    int n=5;
+    int fact=1;
+    for (int  i=1; i<=5; i++) {
+        fact=fact*i;
+    }
+    printf (" factorial:%d",fact);
+}
+
+        
