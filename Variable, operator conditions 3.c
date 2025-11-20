24bd1a7a9663fa3615ba6621fd9a8ce0127e6029
yyamuna07136-art <yyamuna07136@gmail.com>
2025-11-20 07:56:23 +0530

    Create Variable, operator conditions 3.c

---
+++ Variable, operator conditions 3.c
@@ -0,0 +1,15 @@
+// check wheather the number is even or odd
+#include <stdio.h>
+
+int main() {
+    int n;
+    printf(" Enter number:");
+    scanf("%d",&n);
+    if(n % 2 == 0)
+    printf ("Even");
+    else
+    printf ("Odd");
+    
+    return 0;
+}
+    
