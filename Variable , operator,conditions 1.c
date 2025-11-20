31466b408eb72054a3455ebed8fd6c130a34a778
yyamuna07136-art <yyamuna07136@gmail.com>
2025-11-20 07:48:01 +0530

    Create Variable , operator,conditions 1.c

---
+++ Variable , operator,conditions 1.c
@@ -0,0 +1,14 @@
+// find sum ,diff,pro quo of two numbers 
+#include <stdio.h>
+
+int main() {
+    int a, b;
+    printf(" Enter two  numbers:");
+    scanf("%d %d",&a, &b);
+        printf ("Sum = %d\n", a + b);
+        printf ("Difference =  %d\n", a - b);
+        printf ("product = %d\n",a * b);
+        printf ("Quotient = %d\n", a / b);
+return 0;
+    }
+    
