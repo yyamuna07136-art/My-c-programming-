d23f1df547ac9f7a541ff71be7aca397f4f27e89
yyamuna07136-art <yyamuna07136@gmail.com>
2025-11-21 10:07:57 +0530

    Create Malloc.1 free.c

---
+++ Malloc.1 free.c
@@ -0,0 +1,14 @@
+// malloc
+#include <stdio.h>
+#include<stdlib.h>
+
+
+int main() {
+    int * p=(int *)malloc(sizeof(int));
+    * p = 10;
+ 
+    printf(" value = %p\n", * p );
+    free(p);
+
+    return 0;
+}
